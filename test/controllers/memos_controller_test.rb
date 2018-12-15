a62aeef7cc29916c7285f29ddb5549824cc445e5
Matthew Hummer <biggreyson@gmail.com>
2018-12-14 23:33:36 -0700

    add some tests

--- test/controllers/memos_controller_test.rb
+++ test/controllers/memos_controller_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class MemosControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get memos_show_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get memos_create_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get memos_destroy_url
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get memos_show_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get create" do
+  #   get memos_create_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get destroy" do
+  #   get memos_destroy_url
+  #   assert_response :success
+  # end
 
 end
--- test/models/caretaker_test.rb
+++ test/models/caretaker_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class CaretakerTest < ActiveSupport::TestCase
-  test "can string" do
+  test "can be converted to string" do
     matt = caretakers(:matt)
     assert matt.to_s == "Matthew Hummer"
+  end
+
+  test "should have ability to return time zone" do
+    matt = caretakers(:matt)
+    assert matt.timezone == "Mountain Time (US & Canada)"
+  end
+
+  test "can determine UTC offset" do
+    matt = caretakers(:matt)
+    assert matt.utc_offset == -7
   end
 end
