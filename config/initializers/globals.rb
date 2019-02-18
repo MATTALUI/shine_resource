5c5e5616237462ac9a709f437abe54b703ae5759
Matthew Hummer <biggreyson@gmail.com>
2019-02-17 22:53:07 -0700

    improve new client form

--- config/initializers/globals.rb
+++ config/initializers/globals.rb
@@ -1,4 +1,4 @@
-DEFAULT_PHOTO = File.join(Rails.root, '/system/photos/profpic.jpg')
+DEFAULT_PHOTO = '/system/photos/profpic.jpg'
 ORG_COLORS = {
   red:           '#c62828',
   pink:          '#ad1457',
@@ -20,3 +20,4 @@ ORG_COLORS = {
   grey:          '#424242',
   'blue-grey':   '#37474f'
 }.with_indifferent_access
+STATES = {"Alabama" => "AL", "Alaska" => "AK", "Arizona" => "AZ", "Arkansas" => "AR", "California" => "CA", "Colorado" => "CO", "Connecticut" => "CT", "Delaware" => "DE", "District of Columbia" => "DC", "Florida" => "FL", "Georgia" => "GA", "Hawaii" => "HI", "Idaho" => "ID", "Illinois" => "IL", "Indiana" => "IN", "Iowa" => "IA", "Kansas" => "KS", "Kentucky" => "KY", "Louisiana" => "LA", "Maine" => "ME", "Maryland" => "MD", "Massachusetts" => "MA", "Michigan" => "MI", "Minnesota" => "MN", "Mississippi" => "MS", "Missouri" => "MO", "Montana" => "MT", "Nebraska" => "NE", "Nevada" => "NV", "New Hampshire" => "NH", "New Jersey" => "NJ", "New Mexico" => "NM", "New York" => "NY", "North Carolina" => "NC", "North Dakota" => "ND", "Ohio" => "OH", "Oklahoma" => "OK", "Oregon" => "OR", "Pennsylvania" => "PA", "Rhode Island" => "RI", "South Carolina" => "SC", "South Dakota" => "SD", "Tennessee" => "TN", "Texas" => "TX", "Utah" => "UT", "Vermont" => "VT", "Virginia" => "VA", "Washington" => "WA", "West Virginia" => "WV", "Wisconsin" => "WI", "Wyoming" => "WY"}
