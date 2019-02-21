aa444bd7284c9f511fcd87c680b69565d5bdef77
Matthew Hummer <biggreyson@gmail.com>
2019-02-20 20:35:10 -0700

    add creatability of Service Type

--- app/controllers/service_types_controller.rb
+++ app/controllers/service_types_controller.rb
@@ -1,11 +1,13 @@
 class ServiceTypesController < ApplicationController
   before_action :set_service_type, except: [:index, :new, :create]
+  before_action :validate_organization_id, only: [:create, :update]
 
   def index
     @service_types = ServiceType.for_org(current_user.organization_id)
   end
 
   def show
+    raise
   end
 
   def new
@@ -13,19 +15,37 @@ class ServiceTypesController < ApplicationController
   end
 
   def create
+    @service_type = ServiceType.new(service_type_params)
+    if @service_type.save
+      flash[:success] = "Service Type #{@service_type} created."
+    else
+      flash[:error] = "Sorry. Something went wrong."
+    end
+    redirect_to service_types_path
   end
 
   def edit
+    raise
   end
 
   def update
+    raise
   end
 
   def destroy
+    raise
   end
 
   private
+  def service_type_params
+    params.require(:service_type).permit(:name, :active, :organization_id)
+  end
+
   def set_service_type
     @service_type = ServiceType.for_org(current_user.organization_id).find(params[:id])
+  end
+
+  def validate_organization_id
+    head :forbidden unless current_user.organization_id == params[:service_type][:organization_id].presence
   end
 end
